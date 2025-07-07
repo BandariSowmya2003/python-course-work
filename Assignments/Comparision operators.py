3b862d677fb029b69dd09652c98799531465aafa
BandariSowmya2003 <sowmyav574@gmail.com>
2025-07-07 22:22:44 +0530

    fourth program

---
+++ Assignments/Comparision operators.py
@@ -0,0 +1,8 @@
+c=26
+d=53
+print('Equal to(c==d):',c==d)
+print('Not equal to(c!=d):',c!=d)
+print('Greater than(c>d):',c>d)
+print('Less than(c<d):',c<d)
+print('Greater than or equal to(c>=d):',c>=d)
+print('Less than or equal to(c<=d):',c<=d)
