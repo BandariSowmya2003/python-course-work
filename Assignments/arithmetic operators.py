9c2e783c8a1346bd5e39d9f5ddb2191730f65e9a
BandariSowmya2003 <sowmyav574@gmail.com>
2025-07-07 22:10:49 +0530

    third program

---
+++ Assignments/arithmetic operators.py
@@ -0,0 +1,9 @@
+a=20
+b=30
+print('Addition(a+b):',a+b)
+print('Subtraction(a-b):',a-b)
+print('Multiplication(a*b):',a*b)
+print('Division(a/b):',a/b)
+print('Flow division(a//b):',a//b)
+print('Modulus(a%b):',a%b)
+print('Exponentiation(a**b):',a**b)
