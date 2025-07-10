f8222d88dc34fbc49c98b618c13cb8338859f3e2
BandariSowmya2003 <sowmyav574@gmail.com>
2025-07-10 14:56:09 +0530

    operators

---
+++ Class work/#4.7.Bitwise operators.py
@@ -0,0 +1,9 @@
+#7.Bitwise Operators
+x = 7
+y = 4 
+print(x & y) #And=4
+print(x | y) #Or=7
+print(x ^ y) #Xor=3
+print(~x) #Not=-8
+print(x << 1) #Left shift=14
+print(x>>1)#Right shift=3
---
+++ Class work/Strings.py
@@ -0,0 +1,25 @@
+#1.Operations on strings
+#1.1 Concatenation
+str1="Sowmya"
+str2="Bandari"
+result=(str1+ " " + str2)
+print(result)
+
+#1.2 Repetition
+print("Python! "*5)
+
+#1.3 Indexing
+Text="Program"
+print(text[0])
+print(text[-6])
+
+#1.4 Slicing
+print(text[0:3])
+print(text[:4])
+print(text[3:])
+
+#1.5 Membership
+print("Pro" in text)
+print("Java" not in text)
+
+#2.Built in functions
