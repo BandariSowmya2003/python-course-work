202a9a50f885bade39a19c430f6a32c126bea1e7
BandariSowmya2003 <sowmyav574@gmail.com>
2025-07-10 21:17:20 +0530

    variable

---
+++ Class work/2.Variables.py
@@ -0,0 +1,26 @@
+#Basic Variable Assignment
+product_name = "Mobile"
+price = 20000
+in_stock = True
+print(product_name, price, in_stock)
+
+#Multiple Assignment
+a, b, c = 20, 30, 60
+print(a, b, c)
+x = y = z = 150
+print(x, y, z)
+
+#Reassignment
+x = 4
+x = 8
+print(x)
+
+#Swapping Variables
+a, b = 20, 40
+a, b =b, a
+print(a, b)
+
+#Deleting Variables
+x = 150
+print(x)
+del x
