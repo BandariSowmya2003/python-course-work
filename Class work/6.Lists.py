94b3ebc312be16d515ce6fcad375f09f4329c4c8
BandariSowmya2003 <sowmyav574@gmail.com>
2025-07-12 18:17:58 +0530

    tuple

--- Class work/6.Lists.py
+++ Class work/6.Lists.py
@@ -1,52 +1,52 @@
 #1.Creating Lists
 #1.1 Empty list
 list = []
-print(list)
+print(list)#[]
 
 #1.2 List with Elements
 l=[1,3,2,4,5,6]
-print(type(l))
+print(type(l))#<class 'list'>
 l=['apple','banana','mango','cherry']
-print(type(l))
+print(type(l))#<class 'list'>
 l=[5,'Codegnan',2.67,True,3+6j]
-print(type(l))
+print(type(l))#<class 'list'>
 
 #1.3 Nested Lists
 nested_list=[[1,4,2,5,6,], ['p','q','r'],[True,False]]
-print(type(nested_list))
+print(type(nested_list))#<class 'list'>
 
 #2.Accessing Elements
 #2.1 Indexing(positive & negative)
 l=['java','database','python']
-print(l[0])
-print(l[-1])
-print(l[1])
+print(l[0])#java
+print(l[-1])#python
+print(l[1])#database
 
 #2.2 Slicing
 l=[1,2,3,4,5,5,7,8,5,4,3,9]
-print(l[1:5])
-print(l[:4])
-print(l[5:])
-print(l[-2:-5])
-print(l[::-1])
+print(l[1:5])#[2,3,4,5]
+print(l[:4])#[1,2,3,4]
+print(l[5:])#[5,7,8,5,4,3,9]
+print(l[-5:-1])#[8, 5, 4, 3]
+print(l[::-1])#[9, 3, 4, 5, 8, 7, 5, 5, 4, 3, 2, 1]
 
 #3. Modify List
 #3.1 Changing elements
 l=[20,40,60,80]
 l[2]=50
-print(l)
+print(l)#[20,40,50,80]
 
 #3.2 Adding elements
 l=[1,2,3,4,5,5,7,8,5,4,3,9]
 #append
 l.append(10)
-print(l)
+print(l)#[1, 2, 3, 4, 5, 5, 7, 8, 5, 4, 3, 9, 10]
 #insert
 l.insert(1,7)
-print(l)
+print(l)#[1, 7, 2, 3, 4, 5, 5, 7, 8, 5, 4, 3, 9, 10]
 #extend
 l.extend([2])
-print(l)
+print(l)#[1, 7, 2, 3, 4, 5, 5, 7, 8, 5, 4, 3, 9, 10, 2]
 
 #3.3 Removing Elements
 l=[1,2,3,4,5,5,7,8,5,4,3,9]
